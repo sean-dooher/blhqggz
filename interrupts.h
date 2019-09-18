221326cdf205fe862201a893e71de452fe1c7680
Sean Dooher <sean@dooher.net>
2019-09-18 12:31:00 -0700

    Add assembly libraries

--- interrupts.h
+++ interrupts.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "stdint.h"
+#include <stdint.h>
 
 void interrupt_handler(uint64_t *sp, uint32_t mcause, uint64_t *mepc);
-
-
 void supervisor_interrupt_handler(uint64_t *sp, uint32_t mcause, uint64_t *mepc);
---
+++ lib/asm/bootup.s
@@ -0,0 +1,7 @@
+.equ STACK_SIZE, 4096
+
+.equ PMP_R, 0x01
+.equ PMP_W, 0x02
+.equ PMP_X, 0x04
+
+.equ PMP_NAPOT, 0x18
