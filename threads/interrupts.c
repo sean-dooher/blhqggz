f9c3d10ea5b9aff96809a1644c958f5853e97b47
Sean Dooher <sean@dooher.net>
2019-09-24 13:22:26 -0700

    Update interrupt handlers with more useful printfs

--- threads/interrupts.c
+++ threads/interrupts.c
@@ -1,14 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "interrupts.h"
-#include "devices/serial.h"
 
 void
-machine_interrupt_handler(uint64_t *sp, uint32_t mcause, uint64_t *mepc)
+machine_interrupt_handler(uint64_t *sp, uint64_t mcause, uint64_t mstatus, uint64_t *mepc)
 {
+    printf("machine interrupt: \n\tsp: 0x%p\n\tscause: 0x%lx\n\tsstatus: 0x%lx\n\tsepc: 0x%p\n", sp, mcause, mstatus, mepc);
+    while (true);
     return;
 }
 
 void
-supervisor_interrupt_handler(uint64_t *sp, uint32_t mcause, uint64_t *mepc)
+supervisor_interrupt_handler(uint64_t *sp, uint64_t scause, uint64_t sstatus, uint64_t *sepc)
 {
+    printf("supervisor interrupt: \n\tsp: 0x%p\n\tscause: 0x%lx\n\tsstatus: 0x%lx\n\tsepc: 0x%p\n", sp, scause, sstatus, sepc);
+    while (true);
     return;
 }
--- threads/interrupts.h
+++ threads/interrupts.h
@@ -5,4 +5,5 @@
 void machine_interrupt_handler(uint64_t *sp, uint32_t mcause, uint64_t *mepc);
 void supervisor_interrupt_handler(uint64_t *sp, uint32_t mcause, uint64_t *mepc);
 
-void enable_interrupts_s (void);
+void machine_interrupt_handler(uint64_t *sp, uint64_t mcause, uint64_t mstatus, uint64_t *mepc);
+void supervisor_interrupt_handler(uint64_t *sp, uint64_t scause, uint64_t sstatus, uint64_t *sepc);
